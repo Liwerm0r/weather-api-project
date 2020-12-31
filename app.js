a373ea167bb0def96343f542f739ca654c5037fe
Rafał Ostrowidzki <r.ostrowidzki@gmail.com>
2020-12-31 23:22:18 +0100

    add search engine for finding weather for a given city

--- app.js
+++ app.js
@@ -2,24 +2,33 @@ const express = require('express');
 const app = express();
 const https = require('https');
 const port = 3000;
-
-const apiKey = "f882f7f76dcc7fb16df2d7b6b7193350";
-var city = "Warsaw";
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
-  const url = 'https://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=f882f7f76dcc7fb16df2d7b6b7193350&units=metric';
-  https.get(url, (resp) => {
+  res.sendfile(`${__dirname}/index.html`);
+});
 
+app.post("/", (req, res) => {
+  const query = req.body.cityName;
+  const apiKey = "f882f7f76dcc7fb16df2d7b6b7193350";
+  const units = "metric";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${units}`;
+
+  https.get(url, (resp) => {
     resp.on('data', (data) => {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
       const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
       res.write("<p>The weather is currently " + weatherDescription + "</p>")
-      res.write("<h1>The temperature in Warsaw is " + temp + " degrees Celcius.</h1>");
+      res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius.</h1>");
+      res.write(`<img src="http://openweathermap.org/img/wn/${icon}@4x.png" alt="weather-img">`)
+      res.write(`<a href="/">Go back to main page</a>`);
       res.send();
     });
   });
 });
+
 
 app.listen(port, () => {
   console.log("Server is running on port " + port);
